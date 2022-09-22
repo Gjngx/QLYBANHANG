1dc3a6575397de184a367336b0e2f08715f06d31
Gjngx <giangdang2k2@gmail.com>
2022-09-22 21:03:56 +0700

    xong DTO

--- QLYBANHANG/QLYBANHANG/DTO/loaisp.cs
+++ QLYBANHANG/QLYBANHANG/DTO/loaisp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,32 @@ namespace QLYBANHANG.DTO
 {
     public class loaisp
     {
+        public loai(string maloai, string tenloai)
+        {
+            this.Maloai = maloai;
+            this.Tenloai = tenloai;
+        }
 
+        public loai(DataRow row)
+        {
+            this.Maloai = row["maloaisp"].ToString();
+            this.tenloai = row["tenloaisp"].ToString();
+        }
+
+        private string maloai;
+
+        public string Maloai
+        {
+            get { return maloai; }
+            private set { maloai = value; }
+        }
+
+        private string tenloai;
+
+        public string Tenloai
+        {
+            get { return tenloai; }
+            private set { tenloai = value; }
+        }
     }
 }
--- QLYBANHANG/QLYBANHANG/DTO/sanpham.cs
+++ QLYBANHANG/QLYBANHANG/DTO/sanpham.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QLYBANHANG.DTO
 {
-    internal class sanpham
+    public class sanpham
     {
+        public sanpham(string masanpham, string tensanpham, string maloai, float gia)
+        {
+            this.MaSanPham = masanpham;
+            this.MaLoai = maloai;
+            this.TenSanPham = tensanpham;
+            this.Gia = gia;
+        }
+
+        public sanpham(DataRow row)
+        {
+            this.MaSanPham = row["MASP"].ToString();
+            this.MaLoai = row["MAlOAISP"].ToString();
+            this.TenSanPham = row["TENSP"].ToString();
+            this.Gia = (float)Convert.ToDouble(row["GIA"].ToString());
+        }
+
+        private string masanpham;
+
+        public string MaSanPham
+        {
+            get { return masanpham; }
+            set { masanpham = value; }
+        }
+
+        private string tensanpham;
+
+        public string TenSanPham
+        {
+            get { return tensanpham; }
+            set { tensanpham = value; }
+        }
+
+        private string maloai;
+
+        public string MaLoai
+        {
+            get { return maloai; }
+            set { maloai = value; }
+        }
+
+        private float gia;
+
+        public float Gia
+        {
+            get { return gia; }
+            set { gia = value; }
+        }
     }
 }
