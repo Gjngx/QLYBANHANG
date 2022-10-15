06d353cd62ba667ba6cb9772e634bc98e69fa260
quocthinh111 <tp756446@gmail.com>
2022-10-15 19:02:58 +0700

    Them file quanlytkDAO

--- QLYBANHANG/QLYBANHANG/UC/ucquanlytk.cs
+++ QLYBANHANG/QLYBANHANG/UC/ucquanlytk.cs
@@ -1,10 +1,12 @@
 ﻿using DevExpress.XtraEditors;
+using QLYBANHANG.DTO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +16,7 @@ namespace QLYBANHANG.UC
     public partial class ucquanlytk : DevExpress.XtraEditors.XtraUserControl
     {
         private static ucquanlytk instance;
+        private object dstaikhoan;
 
         public static ucquanlytk Instance
         {
@@ -27,6 +30,88 @@ namespace QLYBANHANG.UC
         public ucquanlytk()
         {
             InitializeComponent();
+        }
+
+        private void btnxem_Click(object sender, EventArgs e)
+        {
+            taidstaikhoan();
+        }
+
+        private void taidstaikhoan()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            string tendn = txbtendangnhap.Text;
+            string tennv = txbtennhanvien.Text;
+            int quyen = (int)nudquyen.Value;
+            themtk(tendn, tennv, quyen);
+            taidstaikhoan();
+        }
+
+        private void themtk(string tendn, string tennv, int quyen)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            string tendn = txbtendangnhap.Text;
+            string tennv = txbtennhanvien.Text;
+            int quyen = (int)nudquyen.Value;
+            suatk(tendn, tennv, quyen);
+            taidstaikhoan();
+        }
+
+        private void suatk(string tendn, string tennv, int quyen)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            string tendn = txbtendangnhap.Text;
+            xoatk(tendn);
+            taidstaikhoan();
+        }
+
+        private void xoatk(string tendn)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tendn = txbtendangnhap.Text;
+            string mk = txbmatkhau.Text;
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(mk);
+            byte[] data = new MD5CryptoServiceProvider().ComputeHash(temp);
+
+            string datamk = "";
+
+            foreach (byte b in data)
+            {
+                datamk += b;
+            }
+            datlaimk(tendn, datamk);
+            taidstaikhoan();
+        }
+
+        private void datlaimk(string tendn, string datamk)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Btntim_Click(object sender, EventArgs e)
+        {
+            dstaikhoan = timtaikhoantheoten(txbtim.Text);
+        }
+
+        private object timtaikhoantheoten(string text)
+        {
+            throw new NotImplementedException();
         }
     }
 }
