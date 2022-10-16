4bd1311efdfaf2ec27239a0f4d281e3eec15ff51
thinh <tp756446@gmail.com>
2022-10-16 18:58:34 +0700

    thinh

--- QLYBANHANG/QLYBANHANG/DAO/taikhoanDAO.cs
+++ QLYBANHANG/QLYBANHANG/DAO/taikhoanDAO.cs
@@ -45,5 +45,71 @@ namespace QLYBANHANG.DAO
             int r = DataProvider.Instance.ExecuteNonQuery("EXEC CAPNHATMATKHAU @TENDANGNHAP , @MATKHAU , @MATKHAUMOI", new object[] { tendn, matkhau, matkhaumoi });
             return r > 0;
         }
+
+        public List<taikhoan> xuatdstaikhoan()
+        {
+            List<taikhoan> ds = new List<taikhoan>();
+
+            string query = "SELECT * FROM dbo.TAIKHOAN";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                taikhoan tk = new taikhoan(item);
+                ds.Add(tk);
+            }
+            return ds;
+        }
+
+        public DataTable xuatdstk()
+        {
+            return DataProvider.Instance.ExecuteQuery("SELECT TENDANGNHAP , TENNHANVIEN , QUYEN FROM dbo.TAIKHOAN");
+        }
+
+        public bool themtaikhoan(string tendn, string tennv, int quyen)
+        {
+            string q = string.Format("INSERT dbo.TAIKHOAN( TENDANGNHAP , TENNHANVIEN , QUYEN, PASS  ) VALUES(   '{0}' , N'{1}' , {2}, '20720532132149213101239102231223249249135100218' )", tendn, tennv, quyen);
+            int r = DataProvider.Instance.ExecuteNonQuery(q);
+            return r > 0;
+        }
+
+        public bool suataikhoai(string tendn, string tennv, int quyen)
+        {
+            string q = string.Format("UPDATE dbo.TAIKHOAN SET TENNHANVIEN = N'{0}' , QUYEN = {1} WHERE TENDANGNHAP = '{2}' ", tennv, quyen, tendn);
+            int r = DataProvider.Instance.ExecuteNonQuery(q);
+            return r > 0;
+        }
+        public bool xoataikhoan(string tendn)
+        {
+            string q = string.Format("DELETE dbo.TAIKHOAN WHERE TENDANGNHAP = '{0}'", tendn);
+            int r = DataProvider.Instance.ExecuteNonQuery(q);
+            return r > 0;
+        }
+
+        public bool datlaimatkhau(string tendn, string mk)
+        {
+            string q = string.Format("UPDATE dbo.TAIKHOAN SET PASS = '{0}' WHERE TENDANGNHAP = '{1}'", mk, tendn);
+            int r = DataProvider.Instance.ExecuteNonQuery(q);
+            return r > 0;
+        }
+
+        public List<taikhoan> timtaikhoan(string tennv)
+        {
+            List<taikhoan> ds = new List<taikhoan>();
+
+            string query = string.Format("SELECT * FROM dbo.TAIKHOAN WHERE TENNHANVIEN LIKE N'%{0}%'", tennv);
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                taikhoan tk = new taikhoan(item);
+                ds.Add(tk);
+            }
+
+            return ds;
+        }
+
     }
 }
--- QLYBANHANG/QLYBANHANG/UC/ucquanlytk.cs
+++ QLYBANHANG/QLYBANHANG/UC/ucquanlytk.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.XtraEditors;
+using QLYBANHANG.DAO;
 using QLYBANHANG.DTO;
 using System;
 using System.Collections.Generic;
@@ -13,33 +14,96 @@ using System.Windows.Forms;
 
 namespace QLYBANHANG.UC
 {
-    public partial class ucquanlytk : DevExpress.XtraEditors.XtraUserControl
+    public partial class ucquanlytk : Form
     {
-        private static ucquanlytk instance;
-        private object dstaikhoan;
+        BindingSource dstaikhoan = new BindingSource();
 
-        public static ucquanlytk Instance
-        {
-            get
-            {
-                if (instance == null)
-                    instance = new ucquanlytk();
-                return instance;
-            }
-        }
+        public taikhoan dangnhap;
+
+        public static Control Instance { get; internal set; }
+
         public ucquanlytk()
         {
             InitializeComponent();
+            dgvtaikhoan.DataSource = dstaikhoan;
+            taidstaikhoan();
+            taikhoanbinding();
+        }
+
+        #region methods
+        void taidstaikhoan()
+        {
+            dstaikhoan.DataSource = taikhoanDAO.Instance.xuatdstk();
+        }
+
+        void taikhoanbinding()
+        {
+            txbtendangnhap.DataBindings.Add(new Binding("Text", dgvtaikhoan.DataSource, "tendangnhap"));
+            txbtennhanvien.DataBindings.Add(new Binding("Text", dgvtaikhoan.DataSource, "tennhanvien"));
+            nudquyen.DataBindings.Add(new Binding("Value", dgvtaikhoan.DataSource, "quyen"));
+        }
+
+        void themtk(string tendn, string tennv, int quyen)
+        {
+            if (taikhoanDAO.Instance.themtaikhoan(tendn, tennv, quyen))
+            {
+                MessageBox.Show("Thêm tài khoản thành công");
+            }
+            else
+                MessageBox.Show("Thêm tài khoản thất bại!");
+            taidstaikhoan();
+        }
+
+        void suatk(string tendn, string tennv, int quyen)
+        {
+            if (taikhoanDAO.Instance.suataikhoai(tendn, tennv, quyen))
+            {
+                MessageBox.Show("Cập nhật tài khoản thành công");
+            }
+            else
+                MessageBox.Show("Cập nhật tài khoản thất bại!");
+            taidstaikhoan();
+        }
+
+        void xoatk(string tendn)
+        {
+            if (dangnhap.Tendangnhap.Equals(tendn))
+            {
+                MessageBox.Show("Bạn không thể xóa tài khoản của bạn!");
+                return;
+            }
+            if (taikhoanDAO.Instance.xoataikhoan(tendn))
+            {
+                MessageBox.Show("Xóa tài khoản thành công");
+            }
+            else 
+                MessageBox.Show("Xóa tài khoản thất bại!");
+            taidstaikhoan();
+        }
+        List<taikhoan> timtaikhoantheoten(string tennv)
+        {
+            List<taikhoan> list = taikhoanDAO.Instance.timtaikhoan(tennv);
+            return list;
+        }
+
+        void datlaimk(string tendn, string mk)
+        {
+            if (txbmatkhau.Text.Length < 8)
+            {
+                MessageBox.Show("Mật khẩu cần đủ 8 ký tự!");
+                return;
+            }
+            if (taikhoanDAO.Instance.datlaimatkhau(tendn, mk))
+            {
+                MessageBox.Show("Đặt lại mật khẩu thành công");
+            }
+            else
+                MessageBox.Show("Đặt lại mật khẩu thất bại!");
         }
 
         private void btnxem_Click(object sender, EventArgs e)
         {
             taidstaikhoan();
-        }
-
-        private void taidstaikhoan()
-        {
-            throw new NotImplementedException();
         }
 
         private void btnthem_Click(object sender, EventArgs e)
@@ -51,11 +115,6 @@ namespace QLYBANHANG.UC
             taidstaikhoan();
         }
 
-        private void themtk(string tendn, string tennv, int quyen)
-        {
-            throw new NotImplementedException();
-        }
-
         private void btnsua_Click(object sender, EventArgs e)
         {
             string tendn = txbtendangnhap.Text;
@@ -65,21 +124,11 @@ namespace QLYBANHANG.UC
             taidstaikhoan();
         }
 
-        private void suatk(string tendn, string tennv, int quyen)
-        {
-            throw new NotImplementedException();
-        }
-
         private void btnxoa_Click(object sender, EventArgs e)
         {
             string tendn = txbtendangnhap.Text;
             xoatk(tendn);
             taidstaikhoan();
-        }
-
-        private void xoatk(string tendn)
-        {
-            throw new NotImplementedException();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -99,19 +148,15 @@ namespace QLYBANHANG.UC
             taidstaikhoan();
         }
 
-        private void datlaimk(string tendn, string datamk)
+        private void btntim_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            dstaikhoan.DataSource = timtaikhoantheoten(txbtim.Text);
         }
 
-        private void Btntim_Click(object sender, EventArgs e)
+        private void btnhuy_Click(object sender, EventArgs e)
         {
-            dstaikhoan = timtaikhoantheoten(txbtim.Text);
-        }
 
-        private object timtaikhoantheoten(string text)
-        {
-            throw new NotImplementedException();
         }
     }
+    #endregion
 }
